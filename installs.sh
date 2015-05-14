146acd7318f6d9051da2fc616ff6373a7c6c1f8e
Vianney Tran <vianney.tran@datadoghq.com>
2015-05-14 18:23:49 -0400

    Adding custom theme and plugins

--- installs.sh
+++ installs.sh
@@ -1,7 +1,13 @@
 # ZSH install
 if [ ! -d ~/.oh-my-zsh ]; then
     echo "Installing OMZ"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    curl -L http://install.ohmyz.sh | zsh
+
+    echo "Installing Plugins"
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
+
+	touch .z
 else
     echo "OMZ already installed"
 fi
